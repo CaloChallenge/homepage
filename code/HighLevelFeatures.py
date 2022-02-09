4391a08e86b4b0448ceeccd1789be27f9d063037
Claudius <Claudius.Krause.21@gmail.com>
2022-02-08 22:02:24 -0500

    Fix a few issues so that it runs

--- code/HighLevelFeatures.py
+++ code/HighLevelFeatures.py
@@ -1,18 +1,18 @@
 import numpy as np
 import math
 
-import XMLHandler from XMLHandler
+from XMLHandler import XMLHandler
 
-class HighFeatureFeatures:
+class HighLevelFeatures:
 
-  def __init__(self):
-    
-    xml = XMLHandler()
+  def __init__(self, filename='binning.xml'):
+
+    xml = XMLHandler(filename=filename)
     self.bin_edges = xml.GetBinEdges()
-    self.eta_all_layers, phi_all_layers = xml.GetEtaPhiAllLayers()
+    self.eta_all_layers, self.phi_all_layers = xml.GetEtaPhiAllLayers()
     self.relevantLayers = xml.GetRelevantLayers()
     self.layersBinnedInAlpha = xml.GetLayersWithBinningInAlpha()
-    
+
   def calculate_EC(self, eta, phi, energy):
       eta_EC = (eta * energy).sum(axis=0)/energy.sum(axis=0)
       phi_EC = (phi * energy).sum(axis=0)/energy.sum(axis=0)
@@ -22,71 +22,75 @@ class HighFeatureFeatures:
       eta_width = (eta * eta * energy).sum(axis=0)/energy.sum(axis=0)
       phi_width = (phi * phi * energy).sum(axis=0)/energy.sum(axis=0)
       return eta_width, phi_width
-    
-  def GetECandWidths(eta_layer, phi_layer, energy_layer):
+
+  def GetECandWidths(self, eta_layer, phi_layer, energy_layer):
     eta_EC = 0
     phi_EC = 0
     eta_width = 0
     phi_width = 0
 
-    if (sum(energy_layer)!= 0):
-      eta_EC , phi_EC = self.calculate_EC(eta_layer, phi_layer, energy_layer)
-      eta_width , phi_width = self.calculate_Widths(eta_layer, phi_layer, energy_layer)
+    if sum(energy_layer) != 0:
+      eta_EC, phi_EC = self.calculate_EC(eta_layer, phi_layer, energy_layer)
+      eta_width, phi_width = self.calculate_Widths(eta_layer, phi_layer, energy_layer)
       # The following checks are needed to assure a positive argument to the sqrt, if there is very little energy things can go wrong
       if (eta_EC * eta_EC <= eta_width):
-        eta_width = math.sqrt(eta_width - eta_EC * eta_EC) 
+        eta_width = math.sqrt(eta_width - eta_EC * eta_EC)
       if (phi_EC * phi_EC <= phi_width):
         phi_width = math.sqrt(phi_width - phi_EC * phi_EC)
-        
+
     return eta_EC, phi_EC, eta_width, phi_width
-    
+
   def EvaluateFeatures(self, data):
-    self.E_tot = array( 'f', [ 0 ] )
+    self.E_tot = data.sum(axis=-1)
 
     self.E_layers = {}
-    for l in relevantLayers:
-      E_layer = array( 'f', [ 0 ] )
-      E_layers[l] = E_layer
+    for l in self.relevantLayers:
+      E_layer = data[:, self.bin_edges[l]:self.bin_edges[l+1]].sum(axis=-1)
+      self.E_layers[l] = E_layer
 
     self.EC_etas = {}
     self.EC_phis = {}
     self.width_etas = {}
     self.width_phis = {}
-    for l in layersBinnedInAlpha:
-      EC_eta = array( 'f', [ 0 ] )
-      EC_phi = array( 'f', [ 0 ] )
-      width_eta = array( 'f', [ 0 ] )
-      width_phi = array( 'f', [ 0 ] )
-      EC_etas[l] = EC_eta
-      EC_phis[l] = EC_phi
-      width_etas[l] = width_eta
-      width_phis[l] = width_phi
-    
-    for i in range (0, min(n_events,data.shape[0])):
-      for l in relevantLayers:
-        layer_energy = data[i,bin_edges[l]:bin_edges[l+1]].sum(axis=0)
+    for l in self.layersBinnedInAlpha:
+      #EC_eta = array( 'f', [ 0 ] )
+      #EC_phi = array( 'f', [ 0 ] )
+      #width_eta = array( 'f', [ 0 ] )
+      #width_phi = array( 'f', [ 0 ] )
+      #EC_etas[l] = EC_eta
+      #EC_phis[l] = EC_phi
+      #width_etas[l] = width_eta
+      #width_phis[l] = width_phi
+      self.EC_etas[l] = np.zeros(len(data))
+      self.EC_phis[l] = np.zeros(len(data))
+      self.width_etas[l] = np.zeros(len(data))
+      self.width_phis[l] = np.zeros(len(data))
 
-        E_tot[0] += layer_energy
-        E_layers[l][0] = layer_energy
-                   
-        for l in range(0,24):
-          if l in layersBinnedInAlpha: 
-            EC_etas[l][0], EC_phis[l][0], width_etas[l][0], width_phis[l][0] = GetECandWidths(eta_all_layers[l] , phi_all_layers[l], E_event[bin_edges[l]:bin_edges[l+1]])
-                    
+    for i in range (data.shape[0]):
+      for l in self.relevantLayers:
+        #layer_energy = data[i, self.bin_edges[l]:self.bin_edges[l+1]].sum(axis=0)
+
+        #self.E_tot[0] += layer_energy
+        #self.E_layers[l][0] = layer_energy
+
+        for l in range(0, 24):
+          if l in self.layersBinnedInAlpha:
+            self.EC_etas[l][i], self.EC_phis[l][i], self.width_etas[l][i], self.width_phis[l][i] = self.GetECandWidths(self.eta_all_layers[l], self.phi_all_layers[l], data[i, self.bin_edges[l]:self.bin_edges[l+1]])
+
   def GetEtot(self):
     return self.E_tot
-    
+
   def GetElayers(self):
-    return self.E_layers    
+    return self.E_layers
 
   def GetECetas(self):
     return self.EC_etas
-    
+
   def GetECphis(self):
     return self.EC_phis
-    
+
   def GetWidthEtas(self):
     return self.width_etas
-    
+
   def GetWidthPhis(self):
     return self.width_phis
--- code/XMLHandler.py
+++ code/XMLHandler.py
@@ -4,16 +4,16 @@ import math
 
 class XMLHandler:
 
-  def __init__(self):
-    
-    tree = ET.parse("binning.xml")
+  def __init__(self, filename='binning.xml'):
+
+    tree = ET.parse(filename)
     root = tree.getroot()
-      
+
     self.r_bins = []
     self.a_bins = []
     self.nBinAlphaPerlayer = []
     self.alphaListPerLayer = []
-    
+
     self.r_edges = []
     self.r_midvalue = []
     self.r_midvalueCorrected = []
@@ -24,11 +24,11 @@ class XMLHandler:
     self.phi_edges = []
     self.eta_bins = []
     self.phi_bins = []
-    
+
     self.etaRegion = 0
-    
+
     for particle in root:
-      for layer in region:
+      for layer in particle:
         self.ReadPolarCoordinates(layer)
 
     self.totalBins = 0
@@ -38,7 +38,6 @@ class XMLHandler:
     self.phi_all_layers = []
 
     self.SetEtaAndPhiFromPolar()
-
     self.bin_edges = [0]
     for i in range(len(self.bin_number)):
       self.bin_edges.append(self.bin_number[i] + self.bin_edges[i])
@@ -49,7 +48,7 @@ class XMLHandler:
     str_r = subelem.attrib.get('r_edges')
     r_list = [float(s) for s in str_r.split(',')]
     bins = len(r_list) - 1
-       
+
     self.r_edges.append(r_list)
     self.r_bins.append(bins)
     layer = subelem.attrib.get('id')
@@ -60,23 +59,21 @@ class XMLHandler:
     if bins_in_alpha > 1:
       self.layerWithBinningInAlpha.append(int(layer))
 
-        
   def fill_r_a_lists(self, layer):
     no_of_rbins = self.r_bins[layer]
     list_mid_values = self.r_midvalue[layer]
     list_a_values = self.alphaListPerLayer[layer]
-    
     r_list = []
     a_list = []
     actual_no_alpha_bins = self.nBinAlphaPerlayer[layer][0]
-    for j0 in range(0, actual_no_alpha_bins):       
+    for j0 in range(0, actual_no_alpha_bins):
       for i0 in range (0, no_of_rbins):
         r_list.append(list_mid_values[i0])
         #print("Layer " + str(layer) + " bin in r: " + str(i0) + "/"+str(no_of_rbins))
         #print("N_bin alpha: " + str(j0) + "/"+ str(actual_no_alpha_bins) + " value of list alpha" + str(list_a_values[i0][j0]) + " with lenght " +str(len(list_a_values)))
         a_list.append(list_a_values[i0][j0])
     return r_list, a_list
-    
+
   def get_midpoint(self, arr):
     middle_points = []
     for i in range(len(arr)-1):
@@ -85,23 +82,22 @@ class XMLHandler:
     return middle_points
 
   def SetEtaAndPhiFromPolar(self):
-)
     self.minAlpha = -math.pi
     self.SetNumberOfBins()
-    
+
     #print("Total bins : %d" % self.totalBins)
-            
+
     r_all_layers = []
     alpha_all_layers = []
-    
+
     for layer in range(len(self.r_bins)):
         #print("the layer is : " + str(layer))
-        #print(r_bins[layer]) 
-        #print(a_bins[layer]) 
-        #print(r_midvalue[layer]) 
+        #print(self.r_bins[layer])
+        #print(self.a_bins[layer])
+        #print(self.r_midvalue[layer])
         #print(alpha[layer])
 
-        r_list, a_list = self.fill_r_a_lists(layer)              
+        r_list, a_list = self.fill_r_a_lists(layer)
         r_all_layers.append(r_list)
         alpha_all_layers.append(a_list)
 
@@ -112,9 +108,9 @@ class XMLHandler:
         self.phi_all_layers.append(phi)
 
         #print("the layer is : " + str(layer))
-        #print(eta) 
-        #print(phi) 
-  
+        #print(eta)
+        #print(phi)
+
   def SetNumberOfBins(self):
     for layer in range(len(self.r_bins)):
       #print(str(i) + " " + str(mergeBinAlphaFirstBinR) + " " + str(totalBins))
@@ -124,33 +120,38 @@ class XMLHandler:
 
       bin_no = self.r_bins[layer]*self.a_bins[layer]
       #print(bin_no)
-      centres_alpha = self.get_midpoint(np.linspace(self.minAlpha , math.pi , self.a_bins[layer]+1))
-      for j in range(self.r_bins[layer]):  
+      centres_alpha = self.get_midpoint(np.linspace(self.minAlpha, math.pi, self.a_bins[layer]+1))
+      for j in range(self.r_bins[layer]):
         alphaBinList.append(centres_alpha)
         nBinAlpha.append(self.a_bins[layer])
 
-      self.alphaListPerLayer.append(alphaBinList)
-      self.nBinAlphaPerlayer.append(nBinAlpha)
+      #self.alphaListPerLayer.append(alphaBinList)
+      #self.nBinAlphaPerlayer.append(nBinAlpha)
 
       self.totalBins += bin_no
       self.bin_number.append(bin_no)
       if self.r_bins[layer] > 0:
         self.relevantlayers.append(layer)
-          
+        self.alphaListPerLayer.append(alphaBinList)
+        self.nBinAlphaPerlayer.append(nBinAlpha)
+      else:
+        self.alphaListPerLayer.append([0])
+        self.nBinAlphaPerlayer.append([0])
+
   def GetTotalNumberOfBins(self):
     return self.totalBins
-    
+
   def GetBinEdges(self):
     return self.bin_edges
-  
+
   def GetEtaPhiAllLayers(self):
     return self.eta_all_layers, self.phi_all_layers
-    
+
   def GetRelevantLayers(self):
     return self.relevantlayers
-  
+
   def GetLayersWithBinningInAlpha(self):
     return self.layerWithBinningInAlpha
-    
+
   def GetEtaRegion(self):
     return self.etaRegion
