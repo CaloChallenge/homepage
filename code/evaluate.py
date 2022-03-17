1bb03862240f5e807ff7a5a65f35e0e903712096
Claudius Krause <Claudius.Krause.21@gmail.com>
2022-03-17 11:45:08 -0400

    Refactor evaluation script to require reference dataset from the  beginning

--- code/evaluate.py
+++ code/evaluate.py
@@ -28,6 +28,10 @@ parser = argparse.ArgumentParser(description=('Evaluate calorimeter showers of t
                                               'Fast Calorimeter Challenge 2022.'))
 
 parser.add_argument('--input_file', '-i', help='Name of the input file to be evaluated.')
+parser.add_argument('--reference_file', '-r',
+                    help='Name of the file to be used as reference. Must be .hdf5 when'+\
+                    ' run for the first time, can be the .pkl that is created in the first'+\
+                    ' run for faster runtime in subsequent runs.')
 parser.add_argument('--mode', '-m', default='all',
                     choices=['all', 'avg', 'avg-E', 'hist-p', 'hist-chi', 'hist',
                              'cls-low', 'cls-high'],
@@ -46,14 +50,16 @@ parser.add_argument('--output_dir', default='evaluation_results/',
                     help='Where to store evaluation output files (plots and scores).')
 parser.add_argument('--source_dir', default='source/',
                     help='Folder that contains (soft links to) files required for'+\
-                    ' comparative evaluations.')
+                    ' comparative evaluations (high level features stored in .pkl or '+\
+                    'datasets prepared for classifier runs.).')
 
 
 ########## Functions and Classes ##########
 
-def check_file(given_file, arg):
+def check_file(given_file, arg, which=None):
     """ checks if the provided file has the expected structure based on the dataset """
-    print("Checking if provided file has the correct form ...")
+    print("Checking if {} file has the correct form ...".format(
+        which if which is not None else 'provided'))
     num_features = {'1-photons': 368, '1-pions': 533, '2': 6480, '3': 40500}[arg.dataset]
     num_events = given_file['incident_energies'].shape[0]
     assert given_file['showers'].shape[0] == num_events, \
@@ -64,48 +70,57 @@ def check_file(given_file, arg):
             num_features, given_file['showers'].shape[1]))
 
     print("Found {} events in the file.".format(num_events))
-    print("Checking if provided file has the correct form: DONE \n")
+    print("Checking if {} file has the correct form: DONE \n".format(
+        which if which is not None else 'provided'))
 
-def check_reference(arg):
-    """ checks if reference file for comparisons exist """
-    return os.path.exists(os.path.join(arg.source_dir, 'reference_{}.hdf5'.format(arg.dataset)))
+#def check_reference(arg):
+#    """ checks if reference file for comparisons exist """
+#    return os.path.exists(os.path.join(arg.source_dir, 'reference_{}.hdf5'.format(arg.dataset)))
 
-def check_pickle(arg):
-    """ checks if reference pickle file of high-level features exist """
-    return os.path.exists(os.path.join(arg.source_dir, 'reference_{}.pkl'.format(arg.dataset)))
+#def check_pickle(arg):
+#    """ checks if reference pickle file of high-level features exist """
+#    return os.path.exists(os.path.join(arg.source_dir, 'reference_{}.pkl'.format(arg.dataset)))
 
-def extract_shower_and_energy(given_file, arg):
+def extract_shower_and_energy(given_file, arg, which):
     """ reads .hdf5 file and returns samples and their energy """
-    print("Extracting showers from file...")
+    print("Extracting showers from {} file ...".format(which))
     shower = given_file['showers'][:]
     energy = given_file['incident_energies'][:]
-    print("Extracting showers from file: DONE.\n")
+    print("Extracting showers from {} file: DONE.\n".format(which))
     return shower, energy
 
-def create_reference(arg):
-    """ Create pickle file with high-level features for reference in plots """
-    print("Could not find pickle file with high-level features as reference. Creating it now ...")
-    particle = {'1-photons': 'photon', '1-pions': 'pion',
-                '2': 'electron', '3': 'electron'}[arg.dataset]
-    hlf_ref = HLF.HighLevelFeatures(particle,
-                                    filename='binning_dataset_{}.xml'.format(
-                                        arg.dataset.replace('-', '_')))
-    source_file = h5py.File(os.path.join(arg.source_dir, 'reference_{}.hdf5'.format(arg.dataset)),
-                            'r')
-    reference_showers, reference_energies = extract_shower_and_energy(source_file, arg)
-    hlf_ref.CalculateFeatures(reference_showers)
-    hlf_ref.Einc = reference_energies
-    with open(os.path.join(arg.source_dir, 'reference_{}.pkl'.format(arg.dataset)), 'wb') as file:
-        pickle.dump(hlf_ref, file)
-    print("Creating reference pickle file: DONE.\n")
-    return hlf_ref
+#def create_reference(arg):
+#    """ Create pickle file with high-level features for reference in plots """
+#    print("Could not find pickle file with high-level features as reference. Creating it now ...")
+#    particle = {'1-photons': 'photon', '1-pions': 'pion',
+#                '2': 'electron', '3': 'electron'}[arg.dataset]
+#    hlf_ref = HLF.HighLevelFeatures(particle,
+#                                    filename='binning_dataset_{}.xml'.format(
+#                                        arg.dataset.replace('-', '_')))
+#    source_file = h5py.File(os.path.join(arg.source_dir, 'reference_{}.hdf5'.format(arg.dataset)),
+#                            'r')
+#    reference_showers, reference_energies = extract_shower_and_energy(source_file, arg)
+#    hlf_ref.CalculateFeatures(reference_showers)
+#    hlf_ref.Einc = reference_energies
+#    with open(os.path.join(arg.source_dir, 'reference_{}.pkl'.format(arg.dataset)), 'wb') as file:
+#        pickle.dump(hlf_ref, file)
+#    print("Creating reference pickle file: DONE.\n")
+#    return hlf_ref
 
-def load_reference(arg):
+def load_reference(filename):
     """ Load existing pickle with high-level features for reference in plots """
     print("Loading file with high-level features.")
-    with open(os.path.join(arg.source_dir, 'reference_{}.pkl'.format(arg.dataset)), 'rb') as file:
+    with open(filename, 'rb') as file:
         hlf_ref = pickle.load(file)
     return hlf_ref
+
+def save_reference(ref_hlf, ref_name, arg):
+    """ Saves high-level features class to file """
+    print("Saving file with high-level features.")
+    filename = os.path.splitext(os.path.basename(ref_name))[0] + '.pkl'
+    with open(os.path.join(arg.source_dir, filename), 'wb') as file:
+        pickle.dump(ref_hlf, file)
+    print("Saving file with high-level features DONE.")
 
 def plot_histograms(hlf_class, reference_class, arg):
     """ plots histograms based with reference file as comparison """
@@ -319,9 +334,9 @@ def plot_ECWidthPhis(hlf_class, reference_class, arg):
                 f.write('\n\n')
         plt.close()
 
-def download_source_reference():
-    """ Download dataset needed for reference """
-    raise NotImplementedError()
+#def download_source_reference():
+#    """ Download dataset needed for reference """
+#    raise NotImplementedError()
 
 def separation_power(hist1, hist2, bins):
     """ computes the separation power aka triangular discrimination (cf eq. 15 of 2009.03796)
@@ -340,18 +355,36 @@ if __name__ == '__main__':
     #print(vars(args))
 
     source_file = h5py.File(args.input_file, 'r')
-
-    check_file(source_file, args)
+    check_file(source_file, args, which='input')
 
     if not os.path.isdir(args.output_dir):
         os.makedirs(args.output_dir)
 
-    shower, energy = extract_shower_and_energy(source_file, args)
     particle = {'1-photons': 'photon', '1-pions': 'pion',
                 '2': 'electron', '3': 'electron'}[args.dataset]
     hlf = HLF.HighLevelFeatures(particle,
                                 filename='binning_dataset_{}.xml'.format(
                                     args.dataset.replace('-', '_')))
+
+    if os.path.splitext(args.reference_file)[1] == '.hdf5':
+        print("using .hdf5 reference")
+        reference_file = h5py.File(args.reference_file, 'r')
+        check_file(reference_file, args, which='reference')
+        reference_hlf = HLF.HighLevelFeatures(particle,
+                                              filename='binning_dataset_{}.xml'.format(
+                                                  args.dataset.replace('-', '_')))
+        reference_shower, reference_energy = extract_shower_and_energy(reference_file, args,
+                                                                       which='reference')
+        reference_hlf.Einc = reference_energy
+        save_reference(reference_hlf, args.reference_file, args)
+
+    elif os.path.splitext(args.reference_file)[1] == '.pkl':
+        print("using .pkl file for reference")
+        reference_hlf = load_reference(args.reference_file)
+    else:
+        raise ValueError("reference_file must be .hdf5 or .pkl!")
+
+    shower, energy = extract_shower_and_energy(source_file, args, which='input')
 
     # evaluations:
     if args.mode in ['all', 'avg']:
@@ -361,6 +394,17 @@ if __name__ == '__main__':
                                                         'average_shower_dataset_{}.png'.format(
                                                             args.dataset)),
                                   title="Shower average")
+        if hasattr(reference_hlf, 'avg_shower'):
+            pass
+        else:
+            reference_hlf.avg_shower = reference_shower.mean(axis=0, keepdims=True)
+            save_reference(reference_hlf, args.reference_file, args)
+        _ = hlf.DrawAverageShower(reference_hlf.avg_shower,
+                                  filename=os.path.join(
+                                      args.output_dir,
+                                      'reference_average_shower_dataset_{}.png'.format(
+                                          args.dataset)),
+                                  title="Shower average reference dataset")
         print("Plotting average shower: DONE.\n")
 
     if args.mode in ['all', 'avg-E']:
@@ -381,34 +425,43 @@ if __name__ == '__main__':
             _ = hlf.DrawAverageShower(shower[which_showers],
                                       filename=os.path.join(args.output_dir, filename),
                                       title=plot_title[i])
-        print("Plotting average shower for different energies: DONE.\n")
+            if hasattr(reference_hlf, 'avg_shower_E'):
+                pass
+            else:
+                reference_hlf.avg_shower_E = {}
+            if target_energies[i] in reference_hlf.avg_shower_E:
+                pass
+            else:
+                which_showers = ((reference_hlf.Einc >= target_energies[i]) & \
+                             (reference_hlf.Einc < target_energies[i+1])).squeeze()
+                reference_hlf.avg_shower_E[target_energies[i]] = \
+                    reference_shower[which_showers].mean(axis=0, keepdims=True)
+                save_reference(reference_hlf, args.reference_file, args)
 
-    # any further evaluation needs a reference dataset, check if that exists:
-    if check_reference(args):
-        print("Reference hdf5 file exist in {}, moving on.\n".format(args.source_dir))
-    else:
-        # TODO: ask if download is ok and download
-        raise FileNotFoundError(
-            "Reference hdf5 file does not exist in {}, please provide file {}".format(
-                args.source_dir, 'reference_{}.hdf5'.format(args.dataset)))
+            _ = hlf.DrawAverageShower(reference_hlf.avg_shower_E[target_energies[i]],
+                                      filename=os.path.join(args.output_dir,
+                                                            'reference_'+filename),
+                                      title='reference '+plot_title[i])
+
+        print("Plotting average shower for different energies: DONE.\n")
 
     if args.mode in ['all', 'hist-p', 'hist-chi', 'hist']:
         print("Calculating high-level features for histograms ...")
         hlf.CalculateFeatures(shower)
         hlf.Einc = energy
 
+        if reference_hlf.E_tot is None:
+            reference_hlf.CalculateFeatures(reference_shower)
+            save_reference(reference_hlf, args.reference_file, args)
         print("Calculating high-level features for histograms: DONE.\n")
-        if check_pickle(args):
-            reference = load_reference(args)
-        else:
-            reference = create_reference(args)
+
         if args.mode in ['all', 'hist-chi', 'hist']:
             with open(os.path.join(args.output_dir, 'histogram_chi2_{}.txt'.format(args.dataset)),
                       'w') as f:
                 f.write('List of chi2 of the plotted histograms,'+\
                         ' see eq. 15 of 2009.03796 for its definition.\n')
         print("Plotting histograms ...")
-        plot_histograms(hlf, reference, args)
+        plot_histograms(hlf, reference_hlf, args)
         print("Plotting histograms: DONE. \n")
 
     if args.mode in ['cls-low', 'cls-high']:
